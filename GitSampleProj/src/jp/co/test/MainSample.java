397ed383b1efb7bb1fe4e28e7bb183f16034cc0f
htaki <htaki@10.247.17.231>
2014-09-24 19:17:36 +0900

    test

---
+++ GitSampleProj/src/jp/co/test/MainSample.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package jp.co.test;
+
+/**
+ * @author htaki
+ *
+ */
+public class MainSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("test");
+
+	}
+
+}
